36c6eeab6cc64c1028cfae97ec85727db8e62183
Vitaly <onebeattrue@gmail.com>
2024-02-16 00:31:50 +0300

    Initial commit

---
+++ app/src/main/java/edu/mirea/onebeattrue/customviewcompose/MainActivity.kt
@@ -0,0 +1,26 @@
+package edu.mirea.onebeattrue.customviewcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import edu.mirea.onebeattrue.customviewcompose.ui.theme.CustomViewComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomViewComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
